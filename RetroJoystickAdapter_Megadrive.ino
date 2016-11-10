06c1f36fcde8dfbc5fb7e8755b62cbcc0e0128fb
Jarno Lehtinen <lehtinen@sci.fi>
2016-11-11 00:10:39 +0200

    Update RetroJoystickAdapter_Megadrive.ino

--- RetroJoystickAdapter_Megadrive.ino
+++ RetroJoystickAdapter_Megadrive.ino
@@ -55,7 +55,7 @@ uint8_t plugged2 = 0;
     0x09, 0x04,               /* USAGE (Joystick) */ \
     0xa1, 0x01,               /* COLLECTION (Application) */ \
     0x85, REPORT_ID,          /* REPORT_ID */ \
-    /* 16 Buttons */ \
+    /* 8 Buttons */ \
     0x05, 0x09,               /*   USAGE_PAGE (Button) */ \
     0x19, 0x01,               /*   USAGE_MINIMUM (Button 1) */ \
     0x29, 0x08,               /*   USAGE_MAXIMUM (Button 8) */ \
